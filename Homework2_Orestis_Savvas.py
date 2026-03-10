e837325b05628501c771a93754b9d354409c3119
Orestis769 <163779332+Orestis769@users.noreply.github.com>
2026-03-10 15:03:49 +0000

    test

--- Homework2_Orestis_Savvas.py
+++ Homework2_Orestis_Savvas.py
@@ -20,3 +20,42 @@ if patient['Heart rate'] >= 100:
     patient['diagnosis'] = 'High heart rate'
 else:
     print("Ο καρδιακός σας ρυθμός είναι φυσιολογικός.")
+    
+#Αν έχουν temperature πάνω από 102, αλλά δεν έχουν infection, έχουν πάθει θερμοπληξία.
+
+#Αν έχουν temperature πάνω από 102 και έχουν και infection, τότε έχουν γρίπη.
+
+#Αν έχουν infection αλλά όχι temperature πάνω από 102 τότε είναι έχουν ένα απλό κρύωμα.
+
+#Αν δεν έχουν τίποτα από τα παραπάνω πείτε τους να πάρουν μία ασπιρίνη και να σας τηλεφωνήσουν το πρωί.
+
+if patient['Temperature'] > 102 and patient['Infection'] == 'No':
+    print("Έχετε πάθει θερμοπληξία.")
+    patient['diagnosis'] = 'Heat stroke'
+elif patient['Temperature'] > 102 and patient['Infection'] == 'Yes':
+    print("Έχετε γρίπη.")
+    patient['diagnosis'] = 'Flu'
+elif patient['Infection'] == 'Yes' and patient['Temperature'] <= 102:
+    print("Έχετε ένα απλό κρύωμα.")
+    patient['diagnosis'] = 'Cold'
+else:
+    print("Παρακαλώ πάρτε μία ασπιρίνη και τηλεφωνήστε μου το πρωί.")
+    patient['diagnosis'] = 'No serious illness'
+    
+#Όταν τελειώσετε με την παραπάνω λούπα, τυπώστε: "Το όνομα του ασθενή από το λεξικό, η διάγνωσή σου είναι _______."
+print(f"Το όνομα του ασθενή είναι {patient['name']}, η διάγνωσή σου είναι {patient['diagnosis']}.") 
+#5 Δεύτερο Λεξικό Βρείτε ποιοι από τους επόμενους ασθενείς έχουν πάθει θερμοπληξία.
+#patients_2 = [ {'name': 'Beast', 'Temperature': 99}, {'name': 'Simba', 'Temperature': 105}, {'name': 'Snowwhite', 'Temperature': 98}, {'name': 'Donald', 'Temperature': 99}, {'name': 'Bambi', 'Temperature': 100}, {'name': 'Aladdin', 'Temperature': 104}, {'name': 'Hercules', 'Temperature': 105}, {'name': 'Frollo', 'Temperature': 100}]
+patients_2 = [ {'name': 'Beast', 'Temperature': 99}, {'name': 'Simba', 'Temperature': 105}, {'name': 'Snowwhite', 'Temperature': 98}, {'name': 'Donald', 'Temperature': 99}, {'name': 'Bambi', 'Temperature': 100}, {'name': 'Aladdin', 'Temperature': 104}, {'name': 'Hercules', 'Temperature': 105}, {'name': 'Frollo', 'Temperature': 100}]
+for patient in patients_2:
+    if patient['Temperature'] > 102:
+        print(f"{patient['name']} έχει πάθει θερμοπληξία.") 
+
+#6 Σώστε τις διαγνώσεις σε μια λίστα που θα λέγεται records
+records = []
+for patient in patients_2:  
+    if patient['Temperature'] > 102:
+        records.append({'name': patient['name'], 'diagnosis': 'Heat stroke'})
+    else:
+        records.append({'name': patient['name'], 'diagnosis': 'No serious illness'})
+print(records)
--- mathenwwww/pandasMathenw.py
+++ mathenwwww/pandasMathenw.py
@@ -1,2 +1,30 @@
 import pandas as pd
 
+#%matplotlib inline
+import matplotlib.pyplot as plt
+#https://github.com/Orestis769/Data_journalism_Orestis_Savvas/blob/main/mathenwwww/countries.csv
+df = pd.read_csv('https://raw.githubusercontent.com/Orestis769/Data_journalism_Orestis_Savvas/main/mathenwwww/countries.csv')
+#na tis tiposei oles tis grames olh thn lista
+df.info()
+print(df)
+df.columns
+df.dtypes
+print("The population of the world is: ", df['population'].sum())
+print(df.sort_values(by='population', ascending = False).head())
+
+print("apo katv h lista me ta 10 pio poluplithismena xwra: ")
+print(df.sort_values(by='population', ascending = False).head(10))
+print(df["population"].describe()) 
+
+#df['continent'].value_counts().plot()
+print("diagramma apo edw kai katw")
+print(df['continent'].value_counts())
+
+print(f" edw einai toyto {df["population"].std()}")
+#na ftiaxei png me df['continent'].value_counts().plot()
+df['continent'].value_counts().plot(kind='bar')
+print(f"edw einai {df['continent'].value_counts().head()}")
+plt.title('Number of countries per continent')
+plt.xlabel('Continent')
+plt.ylabel('Number of countries')
+plt.savefig('countries_per_continent.png')
