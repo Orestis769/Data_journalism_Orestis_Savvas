6b5c0ef92b2e44aaac106562be60bf9a600dd8d4
Orestis769 <163779332+Orestis769@users.noreply.github.com>
2026-03-10 17:30:42 +0000

    gay

---
+++ GamwThnAsiaPouNaMeinwV1.py
@@ -0,0 +1,13 @@
+#1 thelw na minw se mia xvra tis asias poy exei panw apo to meso oro life exectensi . pia einai ayth h xwra
+import pandas as pd
+df = pd.read_csv("https://raw.githubusercontent.com/datajour-gr/Data_journalism/master/week3/countries.csv")
+# fitaxe asia_list_xwres
+asia_list_xwres = df[df['continent'] == 'Asia'] 
+
+#1 thelw na minw se mia xvra tis asias poy exei panw apo to meso oro life exectensi . pia einai ayth h xwra
+
+asia_list_xwres_new = asia_list_xwres[asia_list_xwres['gdp_per_capita'] > asia_list_xwres['gdp_per_capita'].mean()] 
+#apo tis asialistnew the ba minw se auth me to megalitero prosdokimo 
+
+print(asia_list_xwres_new.sort_values(by='life_expectancy', ascending=False).head(3))
+
